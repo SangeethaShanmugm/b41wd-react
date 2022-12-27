71c4ac7359f14232beaa38cd2fb852c7afc7cf99
SangeethaShanmugm <sanjuriya22@gmail.com>
2022-12-27 09:55:50 +0530

    added useEffect

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import Badge from "@mui/material/Badge";
 
@@ -6,6 +6,11 @@ export function Counter() {
   // let like = 10;
   const [like, setLike] = useState(0);
   const [dislike, setDislike] = useState(0);
+
+  useEffect(() => {
+    console.log("Total click", like, dislike);
+  }, [like, dislike]);
+
   return (
     <div>
       {/* onClick - camelCase */}
